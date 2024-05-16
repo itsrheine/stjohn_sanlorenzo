6a8b59c582935f217bcdb85e36292a33579c49e7
Rheine N <mtiamsic@gmail.com>
2024-05-16 16:09:08 -0700

    reset to add header.html

--- assets/javascript/script.js
+++
@@ -1,5 +0,0 @@
-function  toggleDropdown() {
-    var dropdownContent =
-    document.getElementById("dropdown-content");
-    dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
-}
